c595b8992ad6abddd8c0566e179bf9897663d850
saadahmedscse <saadahmedscse@gmail.com>
2024-01-11 19:11:29 +0600

    add license in all files

--- src/main/java/com/saadahmedev/base/dto/LoginResponse.java
+++ src/main/java/com/saadahmedev/base/dto/LoginResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 Saad Ahmed
+ * Copyright 2018-2024 Saad Ahmed
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
